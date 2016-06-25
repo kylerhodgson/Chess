7678852f2787a5a74dfc98b6b85c8eb01284eae9
Keeton Hodgson <keeton.hodgson@gmail.com>
2016-06-25 12:16:42 -0600

    view3.py now draws a board with pieces in the center of each grid box that can be moved.

--- View/view3.py
+++ View/view3.py
@@ -12,6 +12,7 @@ from kivy.uix.button import Button
 from math import *
 from kivy.graphics import *
 from kivy.graphics import Color
+from kivy.graphics.vertex_instructions import Rectangle
 
 
 class BoardBox(Rectangle):
@@ -21,23 +22,49 @@ class BoardBox(Rectangle):
 class BoardWidget(FloatLayout):
     def __init__(self, **kwargs):
         super(BoardWidget, self).__init__(**kwargs)
-        pieces = []
-        with self.canvas:
-            size = 70
-            for i in range(8):
-                for j in range(8):
-                    Color(1, 0, 0, 1) if (i+j)%2 == 0 else Color(0,0,1,1)  #  colors range from 0-1 instead of 0-255
-                    Rectangle(pos=(j * size, abs(i-7) * size), size=(size,size))
-                    s = Scatter(do_rotation=False, do_scale=False)
-                    s.set_center_x(j * size + size/2)
-                    s.set_center_y((abs(i-7))*size + size/2)
-                    s.auto_bring_to_front = True
-                    im = Image(source='Images/pawn.png')
-                    s.add_widget(im)
-                    s.id = "Pawn" + str(j) + str(abs(i-7))
-                    s.size = (size/2, size/2)
-                    self.add_widget(s)
-                    pieces += [s]
+        with self.canvas.before:
+            Color(0, 0, 0, 1)
+            self.rectangle = Rectangle(pos=(0, 0), size=self.size)
+            length = self.get_length()
+            self.draw_board(length)
+        self.place_pieces(length)
+
+    def on_size(self, *args):
+        self.rectangle.size = self.size
+        self.clear_widgets()
+        self.canvas.clear()
+        length = self.get_length()
+        with self.canvas.before:
+            self.draw_board(length)
+        self.place_pieces(length)
+
+    def draw_board(self, length):
+        Color(0, 0, 0, 1)
+        Rectangle(pos=(0, 0), size=self.size)
+        for i in range(8):
+            for j in range(8):
+                Color(1, 0, 0, 1) if (i + j) % 2 == 0 else Color(0, 0, 1, 1) # colors range from 0-1 instead of 0-255
+                Rectangle(pos=(j * length / 8, abs(i - 7) * length / 8),
+                          size=(length / 8, length / 8))
+
+    def place_pieces(self, length):
+        for i in range(8):
+            for j in range(8):
+                s = Scatter(do_rotation=False, do_scale=False)
+                s.size_hint = None, None
+                s.size = length/8, length/8
+                s.center = (j * length/8 + length / 16, (abs(i - 7)) * length/8 + length / 16)
+                s.auto_bring_to_front = True
+                im = Image(source='Images/pawn.png')
+                im.size_hint = None, None
+                im.size = length / 8, length / 8
+                im.center = im.size[0]/2, im.size[1]/2
+                s.add_widget(im)
+                s.id = "Pawn" + str(j) + str(abs(i - 7))
+                self.add_widget(s)
+
+    def get_length(self):
+        return self.rectangle.size[0] if self.rectangle.size[0] < self.rectangle.size[1] else self.rectangle.size[1]
 
 
 class Chess3(App):
